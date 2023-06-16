e56ccc271b04de9938f42a7bb0183c1c4d4e770e
jiechen <1487503910@qq.com>
2023-06-16 23:19:45 +0800

    feat(test): effect callback

--- core-implementation/reactivity/test/effect.spec.ts
+++ core-implementation/reactivity/test/effect.spec.ts
@@ -18,4 +18,19 @@ describe('effect', () => {
     user.age++;
     expect(nextAge).toBe(12);
   })
+
+  it('effect should return runner when call it', () => {
+    // effect 返回一个 runner function，这个 function 返回值是 fn 的返回值
+    let foo = 10;
+    const runner = effect(() => {
+      foo++;
+      return 'foo';
+    })
+
+    expect(foo).toBe(11);
+    
+    const r = runner();
+    expect(foo).toBe(12);
+    expect(r).toBe('foo');
+  })
 })
